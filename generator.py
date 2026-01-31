2302086a240c6ce75f9517eed682a3c8606dad47
andreasplenge <andreas.p.andreasen@gmail.com>
2026-01-31 08:39:08 +0100

    fix \

--- generator.py
+++ generator.py
@@ -13,106 +13,106 @@ with open("cv.tex", "w") as out:
 
 
     out.write(f"""
-    \documentclass[11pt, a4paper]{{article}} 
+    \\documentclass[11pt, a4paper]{{article}} 
 
-    \usepackage[T1]{{fontenc}}     
-    \usepackage[utf8]{{inputenc}}  
-    \usepackage[english]{{babel}}  
-    \usepackage[left = 0mm, right = 0mm, top = 0mm, bottom = 0mm]{{geometry}}
-    \usepackage[stretch = 25, shrink = 25, tracking=true, letterspace=30]{{microtype}}  
-    \usepackage{{graphicx}}        
-    \usepackage{{xcolor}}          
-    \usepackage{{marvosym}}        
+    \\usepackage[T1]{{fontenc}}     
+    \\usepackage[utf8]{{inputenc}}  
+    \\usepackage[english]{{babel}}  
+    \\usepackage[left = 0mm, right = 0mm, top = 0mm, bottom = 0mm]{{geometry}}
+    \\usepackage[stretch = 25, shrink = 25, tracking=true, letterspace=30]{{microtype}}  
+    \\usepackage{{graphicx}}        
+    \\usepackage{{xcolor}}          
+    \\usepackage{{marvosym}}        
 
-    \usepackage{{enumitem}}        
-    \setlist{{parsep = 0pt, topsep = 0pt, partopsep = 1pt, itemsep = 1pt, leftmargin = 6mm}}
+    \\usepackage{{enumitem}}        
+    \\setlist{{parsep = 0pt, topsep = 0pt, partopsep = 1pt, itemsep = 1pt, leftmargin = 6mm}}
 
-    \usepackage{{FiraSans}}        
-    \renewcommand{{\familydefault}}{{\sfdefault}}
+    \\usepackage{{FiraSans}}        
+    \\renewcommand{{\\familydefault}}{{\\sfdefault}}
 
-    \definecolor{{cvblue}}{{HTML}}{{304263}}
+    \\definecolor{{cvblue}}{{HTML}}{{304263}}
 
     %%%%%%% USER-DEFINED COMMANDS %%%%%%%%%%%%%%%%%%%%%%%%%%%
-    \newcommand{{\dates}}[1]{{\hfill\mbox{{\textbf{{#1}}}}}} 
-    \newcommand{{\is}}{{\par\vskip.5ex plus .4ex}} 
-    \newcommand{{\smaller}}[1]{{\small$\diamond$\ #1}}
-    \newcommand{{\headleft}}[1]{{\vspace*{{3ex}}\textsc{{\textbf{{#1}}}}\par%
-        \vspace*{{-1.5ex}}\hrulefill\par\vspace*{{0.7ex}}}}
-    \newcommand{{\headright}}[1]{{\vspace*{{2.5ex}}\textsc{{\Large\color{{cvblue}}#1}}\par%
-        \vspace*{{-2ex}}{{\color{{cvblue}}\hrulefill}}\par}}
-    \newcommand{{\grayed}}[1]{{\hfill\mbox{{\textcolor{{gray}}{{#1}}}}}}
+    \\newcommand{{\\dates}}[1]{{\\hfill\\mbox{{\\textbf{{#1}}}}}} 
+    \\newcommand{{\\is}}{{\\par\\vskip.5ex plus .4ex}} 
+    \\newcommand{{\\smaller}}[1]{{\\small$\\diamond$\\ #1}}
+    \\newcommand{{\\headleft}}[1]{{\\vspace*{{3ex}}\\textsc{{\\textbf{{#1}}}}\\par%
+        \\vspace*{{-1.5ex}}\\hrulefill\\par\\vspace*{{0.7ex}}}}
+    \\newcommand{{\\headright}}[1]{{\\vspace*{{2.5ex}}\\textsc{{\\Large\\color{{cvblue}}#1}}\\par%
+        \\vspace*{{-2ex}}{{\\color{{cvblue}}\\hrulefill}}\\par}}
+    \\newcommand{{\\grayed}}[1]{{\\hfill\\mbox{{\\textcolor{{gray}}{{#1}}}}}}
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    \usepackage[colorlinks = true, urlcolor = white, linkcolor = white]{{hyperref}}
+    \\usepackage[colorlinks = true, urlcolor = white, linkcolor = white]{{hyperref}}
 
-    \begin{{document}}
+    \\begin{{document}}
 
     % Style definitions -- removes unnecessary space and explicitly adds breaks
-    \setlength{{\topskip}}{{0pt}}
-    \setlength{{\parindent}}{{0pt}}
-    \setlength{{\parskip}}{{0pt}}
-    \setlength{{\fboxsep}}{{0pt}}
-    \pagestyle{{empty}}
-    \raggedbottom
+    \\setlength{{\\topskip}}{{0pt}}
+    \\setlength{{\\parindent}}{{0pt}}
+    \\setlength{{\\parskip}}{{0pt}}
+    \\setlength{{\\fboxsep}}{{0pt}}
+    \\pagestyle{{empty}}
+    \\raggedbottom
 
-    \begin{{minipage}}[t]{{0.33\textwidth}} %% Left column -- outer definition
+    \\begin{{minipage}}[t]{{0.33\\textwidth}} %% Left column -- outer definition
     % Left column -- top dark rectangle
-    \colorbox{{cvblue}}{{\begin{{minipage}}[t][5mm][t]{{\textwidth}}\null\hfill\null\end{{minipage}}}}
+    \\colorbox{{cvblue}}{{\\begin{{minipage}}[t][5mm][t]{{\\textwidth}}\\null\\hfill\\null\\end{{minipage}}}}
 
-    \vspace{{-.2ex}} 
-    \colorbox{{cvblue!90}}{{\color{{white}}  
-    \kern0.09\textwidth\relax
-    \begin{{minipage}}[t][293mm][t]{{0.82\textwidth}}
-    \raggedright
-    \vspace*{{2.5ex}}
+    \\vspace{{-.2ex}} 
+    \\colorbox{{cvblue!90}}{{\\color{{white}}  
+    \\kern0.09\\textwidth\\relax
+    \\begin{{minipage}}[t][293mm][t]{{0.82\\textwidth}}
+    \\raggedright
+    \\vspace*{{2.5ex}}
 
-    \Large Andreas \textbf{{\textsc{{Plenge}}}} \normalsize 
+    \\Large Andreas \\textbf{{\\textsc{{Plenge}}}} \\normalsize 
 
-    \vspace*{{0.5ex}} 
+    \\vspace*{{0.5ex}} 
 
-    \headleft{{Algo \& Quant Engineer}}
+    \\headleft{{Algo \\& Quant Engineer}}
     Engineer with experience in stabilizing the energy grid. I combine a hybrid background in mathematics and software engineering to design scalable analytical tools and model complex systems. I have a strong foundation in dynamical systems, probability, and programming, and I am motivated by solving data-driven problems in performance-sensitive environments, particularly those involving uncertainty, forecasting, and optimization.
 
-    \headleft{{Contact Information}}
-    \href{{mailto:andreas.p.andreasen@gmail.com}}
-    {{andreas.p.andreasen@gmail.com}}\\
-    +45\,52\,99\,06\,66 \\
-    \href{{https://www.linkedin.com/in/andreasplenge}}{{Linkedin/andreasplenge}}\\
-    \normalsize
+    \\headleft{{Contact Information}}
+    \\href{{mailto:andreas.p.andreasen@gmail.com}}
+    {{andreas.p.andreasen@gmail.com}}\\\\
+    +45\\,52\\,99\\,06\\,66 \\\\
+    \\href{{https://www.linkedin.com/in/andreasplenge}}{{Linkedin/andreasplenge}}\\\\
+    \\normalsize
 
-    \headleft{{Languages}}
-    \begin{{itemize}}
-    \item Danish --- Native
-    \item English --- Fluent
-    \end{{itemize}}
+    \\headleft{{Languages}}
+    \\begin{{itemize}}
+    \\item Danish --- Native
+    \\item English --- Fluent
+    \\end{{itemize}}
 
-    \headleft{{Programming Languages}}
-    \begin{{itemize}}
-    \item Python \& R\item noSQL \& SQL\item TypeScript \& JavaScript\item DAX \& M (Power Query)\item Java
-    \end{{itemize}}
+    \\headleft{{Programming Languages}}
+    \\begin{{itemize}}
+    \\item Python \\& R\\item noSQL \\& SQL\\item TypeScript \\& JavaScript\\item DAX \\& M (Power Query)\\item Java
+    \\end{{itemize}}
 
-    \headleft{{Tools \& Frameworks}}
-    \begin{{itemize}}
-    \item Power BI\item Excel\item Pandas, Numpy \& TensorFlow\item Power Automate
-    \end{{itemize}}
+    \\headleft{{Tools \\& Frameworks}}
+    \\begin{{itemize}}
+    \\item Power BI\\item Excel\\item Pandas, Numpy \\& TensorFlow\\item Power Automate
+    \\end{{itemize}}
 
-    \headleft{{Skills}}
-    \begin{{itemize}}
-    \item Algorithm Optimization\item Query Optimization\item Data Visualization\item Programming \item Problem-Solving
-    \end{{itemize}}
-    \headleft{{}}
-    \textbf{{References are available upon request}}
+    \\headleft{{Skills}}
+    \\begin{{itemize}}
+    \\item Algorithm Optimization\\item Query Optimization\\item Data Visualization\\item Programming \\item Problem-Solving
+    \\end{{itemize}}
+    \\headleft{{}}
+    \\textbf{{References are available upon request}}
 
-    \end{{minipage}}%
-    \kern0.09\textwidth\relax}}
+    \\end{{minipage}}%
+    \\kern0.09\\textwidth\\relax}}
     
-    \end{{minipage}}%
-    \hskip2.5em
-    \begin{{minipage}}[t]{{0.56\textwidth}}
-    \setlength{{\parskip}}{{0.8ex}}
+    \\end{{minipage}}%
+    \\hskip2.5em
+    \\begin{{minipage}}[t]{{0.56\\textwidth}}
+    \\setlength{{\\parskip}}{{0.8ex}}
 
-    \vspace{{2ex}}
+    \\vspace{{2ex}}
 
     """)
 
@@ -124,19 +124,19 @@ with open("cv.tex", "w") as out:
 
 
     out.write(f"""
-    {{\headright{{Work Experience}}}}\\\\
+    {{\\headright{{Work Experience}}}}\\\\
     """)
 
     for experience in experiences:
         out.write(formatExperience(experience))
 
     out.write(f"""
-    {{\headright{{Education}}}}\\\\
+    {{\\headright{{Education}}}}\\\\
     """)
     for education in educations:
         out.write(formatEducation(education))
 
     out.write(f"""
-    \end{{minipage}}\\\\
-    \end{{document}}\\\\
+    \\end{{minipage}}\\\\
+    \\end{{document}}\\\\
     """)

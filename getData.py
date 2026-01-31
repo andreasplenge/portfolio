d8e75a1f203feea7bc9bdced73198d07b1a4897d
andreasplenge <andreas.p.andreasen@gmail.com>
2026-01-31 10:51:49 +0100

    fix path

--- getData.py
+++ getData.py
@@ -4,15 +4,12 @@ import yaml
 def getData(type):
     data = []
 
-    if type == "information":
-        path = f'data/general/{type}'
+    if type == "information" or type == "qualifications":
+        path = Path(f'data/general/{type}.yaml')
         with path.open() as f:
             data.extend(yaml.safe_load(f))
 
-    if type == "qualifications":
-        path = f'data/general/{type}'
-        with path.open() as f:
-            data.extend(yaml.safe_load(f))
+
     if type in ["experience", "education", "coursework"]:
         for path in Path(f'data/{type}').glob("*.yaml"):
             with path.open() as f:

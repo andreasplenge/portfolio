bc5f88d9d8d99061690d1048a5740f9a4b65c1b6
andreasplenge <andreas.p.andreasen@gmail.com>
2026-01-31 11:00:52 +0100

    fix how to read signle objects

--- getData.py
+++ getData.py
@@ -2,14 +2,13 @@ from pathlib import Path
 import yaml
 
 def getData(type):
-    data = []
-
     if type == "information" or type == "qualifications":
         path = Path(f'data/general/{type}.yaml')
         with path.open() as f:
-            data.extend(yaml.safe_load(f))
+            return yaml.safe_load(f)
 
-
+    data = []
+    
     if type in ["experience", "education", "coursework"]:
         for path in Path(f'data/{type}').glob("*.yaml"):
             with path.open() as f:

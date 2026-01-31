558144ce34344fe8ca6c7d9ab10ed1af0a44cbe6
andreasplenge <andreas.p.andreasen@gmail.com>
2026-01-31 11:06:19 +0100

    fix

--- getData.py
+++ getData.py
@@ -2,18 +2,22 @@ from pathlib import Path
 import yaml
 
 def getData(type):
-    data = []
-    if type == "information" or type == "qualifications":
+    data = None
+
+    dataIsSingleObject = type == "information" or type == "qualifications"
+
+    if dataIsSingleObject:
         path = Path(f'data/general/{type}.yaml')
         with path.open() as f:
             data = yaml.safe_load(f)
 
     if type in ["experience", "education", "coursework"]:
+        data = []
         for path in Path(f'data/{type}').glob("*.yaml"):
             with path.open() as f:
                 data.extend(yaml.safe_load(f))
 
-    return sorted(
+    return data if dataIsSingleObject else sorted(
         data,
         key=lambda x: x.get("id", 0),
         reverse=True
